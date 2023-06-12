755e2106e552a991c252799ac21828b12511aac6
Abdullah Motiwala <abdullah.motiwala@takafulbazaar.pk>
2023-06-12 13:48:10 +0500

    no cache added in response

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,9 +4,15 @@ import { connectToDB } from "@utils/database"
 export const GET = async (req) => {
     try {
         await connectToDB()
-        const prompts = await Prompt.find({}).populate("creator")
-        return new Response(JSON.stringify(prompts), { status: 200 })
 
+        const prompts = await Prompt.find({}).populate("creator")
+        const response = new Response(JSON.stringify(prompts), { status: 200 })
+
+        response.headers.set('Cache-Control', 'no-store, must-revalidate')
+        response.headers.set('Pragma', 'no-cache')
+        response.headers.set('Expires', '0')
+
+        return response
     } catch (err) {
         return new Response("data fetching failed", { status: 500 })
     }
