ef2ebf47f148413ae49b71a0a13cfe5a17288702
Abdullah Motiwala <abdullah.motiwala@takafulbazaar.pk>
2023-06-12 15:27:23 +0500

    funciton added of connectToDb

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,25 +1,18 @@
 import Prompt from "@models/prompt"
 import { connectToDB } from "@utils/database"
-import mongoose from "mongoose"
 
 export const GET = async (req) => {
     try {
 
-        // await connectToDB()
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "promptopia",
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await connectToDB()
 
         const prompts = await Prompt.find({}).populate("creator")
         const response = new Response(JSON.stringify(prompts), { status: 200 })
 
-        response.headers.set('Cache-Control', 'no-store')
+        response.headers.set('Cache-Control', 'no-store, must-revalidate')
 
         return response
     } catch (err) {
-        console.log(err);
         return new Response("data fetching failed", { status: 500 })
     }
 }
