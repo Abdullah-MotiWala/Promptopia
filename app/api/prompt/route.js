c943de5e52b1c7f96d9f16bf105a3c7f37951845
Abdullah Motiwala <abdullah.motiwala@takafulbazaar.pk>
2023-06-12 15:18:08 +0500

    connect to db func changed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,7 +4,12 @@ import { connectToDB } from "@utils/database"
 export const GET = async (req) => {
     try {
 
-        await connectToDB()
+        // await connectToDB()
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "promptopia",
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
 
         const prompts = await Prompt.find({}).populate("creator")
         const response = new Response(JSON.stringify(prompts), { status: 200 })
